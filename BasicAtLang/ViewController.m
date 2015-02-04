5e56b9e62ba6eb4ea03418d403201e91c6005c70
Ethan Westering <ethan.westering@banno.com>
2015-02-03 21:06:56 -0600

    Initial Commit

---
+++ BasicAtLang/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BasicAtLang
+//
+//  Created by Ethan Westering on 2/3/15.
+//  Copyright (c) 2015 EthanWestering. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
